7ac1eb93f0440d5babbdeadef5afa7e9bc428424
danhphan <danh.phan.mq@gmail.com>
2021-08-08 13:33:06 +1000

    add try except for los api

--- notebooks/here_los_utils.py
+++ notebooks/here_los_utils.py
@@ -13,8 +13,11 @@ def get_info(los_res):
     Return
         (trip_duration, trip_length)
     """
-    res = los_res.routes[0]['sections'][0]['summary']
-    return res['duration'], res['length']
+    try:
+        res = los_res.routes[0]['sections'][0]['summary']
+        return res['duration'], res['length']
+    except:
+        return 0, 0
 
 def collect_here_los(ls, orig, dest, departure_time=None):
     """
@@ -27,17 +30,20 @@ def collect_here_los(ls, orig, dest, departure_time=None):
         departure_time: the trip's departure time in datetime format
     Return
         [car_duration, car_length, cycle_duration, cycle_length, walk_duration, walk_length]
-    """    
-    # Get car duration and length
-    car_res = ls.car_route(origin=orig, destination=dest, departure_time=departure_time, 
-                        return_results=[ROUTING_RETURN.summary])
-    car_dur, car_len = get_info(car_res)
-    # Get cycle duration and length
-    cycle_res = ls.bicycle_route(origin=orig, destination=dest, departure_time=departure_time, 
-                        return_results=[ROUTING_RETURN.summary])
-    cycle_dur, cycle_len = get_info(cycle_res)
-    # Get walk duration and length
-    walk_res = ls.pedestrian_route(origin=orig, destination=dest, departure_time=departure_time, 
-                        return_results=[ROUTING_RETURN.summary])
-    walk_dur, walk_len = get_info(walk_res)
-    return [car_dur, car_len, cycle_dur, cycle_len, walk_dur, walk_len]    
+    """
+    try:
+        # Get car duration and length
+        car_res = ls.car_route(origin=orig, destination=dest, departure_time=departure_time, 
+                            return_results=[ROUTING_RETURN.summary])
+        car_dur, car_len = get_info(car_res)
+        # Get cycle duration and length
+        cycle_res = ls.bicycle_route(origin=orig, destination=dest, departure_time=departure_time, 
+                            return_results=[ROUTING_RETURN.summary])
+        cycle_dur, cycle_len = get_info(cycle_res)
+        # Get walk duration and length
+        walk_res = ls.pedestrian_route(origin=orig, destination=dest, departure_time=departure_time, 
+                            return_results=[ROUTING_RETURN.summary])
+        walk_dur, walk_len = get_info(walk_res)
+        return [car_dur, car_len, cycle_dur, cycle_len, walk_dur, walk_len]    
+    except:
+        return [0, 0, 0, 0, 0, 0]
